935b443d66523ed3e7796f0c1f2f98d0c1e46e80
Ganesh Kumar <baduguganeshkumar006@gmail.com>
2025-10-25 15:43:21 +0530

    Initial commit

---
+++ src/utils/storage.js
@@ -0,0 +1,10 @@
+const STORAGE_KEY = "users";
+
+export const getUsers = () => {
+  const users = localStorage.getItem(STORAGE_KEY);
+  return users ? JSON.parse(users) : [];
+};
+
+export const saveUsers = (users) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(users));
+};
